cbfa5b8bcc17ba55fde6f2f49bfaeb088b73f8f9
kingsley young <kingsleyyoung27@gmail.com>
2020-10-23 06:07:10 -0400

    uploading

---
+++ constant.py
@@ -0,0 +1,4 @@
+
+FOLDER_PATH = "C:/Users/black/Desktop/imageconpy\\"
+FOLDER_PATH1 = "C:/Users/black/Desktop/imageconpy"
+DEST_PATH = "C:/Users/black/Desktop/imageconpy/jgpImg"
--- image.py
+++ image.py
@@ -1,6 +1,74 @@
-#adding the Python Imaging Library aka pillow
+import sys
+import time
+import logging
+import glob, os, shutil
+from constant import FOLDER_PATH, FOLDER_PATH1, DEST_PATH
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from PIL import Image
-#
-im = Image.open("poside.webp").convert("RGB")
-#
-im.save("poside.jpg", "jpeg")
+#constants 
+
+def converter():
+    for img in os.listdir(FOLDER_PATH):
+        if img.endswith(".webp"):
+            for file in glob.glob("*.webp"):
+                im = Image.open(file)
+                rgb_im =im.convert("RGB")
+                rgb_im.save(file.replace("webp", "jpg"), quality=95)
+        #print(img)
+
+def copy():
+    print("copy def")
+    for item in os.listdir(FOLDER_PATH1):
+        print("fold dir")
+        if not os.path.isfile(item):
+            print("is the file here")
+            for file in os.listdir(item):
+                print(file, "i see")
+                #print("listdir")
+                if file.endswith(".jpg"):
+                    print("files ending in jpg")
+                    shutil.move(FOLDER_PATH1, file, DEST_PATH)
+                print(file, "i see 2")
+
+#deleting .webp img
+
+
+#making functions to display actions 
+def on_created(event):
+    print("Created")
+def on_deleted(event):
+    print("Deleted")
+def on_modified(event):
+    print("Modified")
+def on_moved(event):
+    print("Moved")
+
+if __name__ == "__main__":
+
+    event_handler = FileSystemEventHandler()
+    #calling event handler fuctions 
+    event_handler.on_created = on_created
+    event_handler.on_deleted = on_deleted
+    event_handler.on_modified = on_modified
+    event_handler.on_moved = on_moved
+    #
+    #defining path
+    path = "C:/Users/black/Desktop/imageconpy"
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    
+    # the loop thats watching the folder
+    try:
+        print("moniroting")
+        while True:
+            converter()
+            copy()  
+            time.sleep(4)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        observer.stop()
+               
+        print("bye")
+    observer.join()
